0f5950a1c448559057f65c6ee27a2a434e8837fc
pdolinic <71389319+pdolinic@users.noreply.github.com>
2022-08-04 14:36:37 +0200

    Update icinga-pumper.sh

--- rulesets/central/icinga-pumper.sh
+++ rulesets/central/icinga-pumper.sh
@@ -9,9 +9,11 @@
 #Exit Codes
 #Exit_Codes=" >> 0:OK, 1:Warn, 2:Critical, 3:Unkown << "
 
-username_path= /home/$(/usr/bin/id -u -n)/LN/icinga-ubsc/ubsc-generic.crt
+username_path_cert= /home/$(/usr/bin/id -u -n)/LN/icinga-ubsc/ubsc-generic.crt
+username_path_key= /home/$(/usr/bin/id -u -n)/LN/icinga-ubsc/ubsc-generic.key
 
-icinga_passive_mic= /usr/bin/curl -k -S -s -i --cert $username_path --key $username_path -H 'Accept: application/json' -X POST 'https://10.0.0.1:5665/v1/actions/process-check-result' -d '{ "type": "Service", "filter": "host.name==\"vx\" && service.name==\"icinga-security\"", "exit_status": 2, "plugin_output": "Critical Security-Warning triggered", "pretty":true }'
+
+icinga_passive_mic= /usr/bin/curl -k -S -s -i --cert $username_path_cert --key $username_path_key -H 'Accept: application/json' -X POST 'https://10.0.0.1:5665/v1/actions/process-check-result' -d '{ "type": "Service", "filter": "host.name==\"vx\" && service.name==\"icinga-security\"", "exit_status": 2, "plugin_output": "Critical Security-Warning triggered", "pretty":true }'
 icinga_mic= /usr/bin/grep -e "module=Microphone, status=ON" /var/log/detector.log
 
 
