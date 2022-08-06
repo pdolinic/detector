0919a14a40b810d3def5903cde8002ecf8bd0fa2
Patrick <pdolinic@netways.de>
2022-08-06 17:05:00 +0200

    smaller fixes

--- rulesets/central/dnsip.sh
+++ rulesets/central/dnsip.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+dnsip(){
 /usr/bin/systemd-resolve --status | /usr/bin/grep "Current DNS Server" | /usr/bin/cut -d ':' -f 2-3 | ts '[%Y-%m-%d %H:%M:%S] module=DNSIP output='  >> /var/log/detector.log
-
+}
+dnsip
--- rulesets/central/inotifywait.sh
+++ rulesets/central/inotifywait.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sec_inotifywatch () {
-/usr/bin/inotifywait -m -e OPEN -e CLOSE /usr/bin/whoami /usr/bin/sudo | ts '[%Y-%m-%d %H:%M:%S] module=inotifywait output=' >> /var/log/detector.log &
+inotifywait () {
+/usr/bin/inotifywait -m -e OPEN -e CLOSE /usr/bin/whoami /usr/bin/sudo | ts '[%Y-%m-%d %H:%M:%S] module=inotifywait output=' >> /var/log/detector.log 
+}
 
-sec_inotifywatch
+inotifywait
