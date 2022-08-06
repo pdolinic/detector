ac74bd89dfd90b0ff842ce99500d956c227a1ef2
Patrick <pdolinic@netways.de>
2022-08-06 17:16:23 +0200

    smaller fixes

--- rulesets/central/inotifywait.sh
+++ rulesets/central/inotifywait.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 inotifywait () {
-/usr/bin/inotifywait -m -e OPEN -e CLOSE /usr/bin/whoami /usr/bin/sudo | ts '[%Y-%m-%d %H:%M:%S] module=inotifywait output=' >> /var/log/detector.log 
+/usr/bin/inotifywait -m -e OPEN -e CLOSE /usr/bin/whoami /usr/bin/sudo | ts '[%Y-%m-%d %H:%M:%S] module=inotifywait output=' >> /var/log/detector.log &
 }
 
 inotifywait
--- rulesets/central/tracee.sh
+++ rulesets/central/tracee.sh
@@ -2,7 +2,7 @@
 
 #Currently deactivated for testing
 
-#docker run --name tracee --rm -it --pid=host --cgroupns=host --privileged -v /etc/os-release:/etc/os-release-host:ro -e LIBBPFGO_OSRELEASE_FILE=/etc/os-release-host aquasec/tracee:latest --rules TRC-2 --rules TRC-6 --rules TRC-7 --rules TRC-15 | ts '[%Y-%m-%d %H:%M:%S] module=Tracee output=' >> /var/log/detector.log
+docker run --name tracee --rm -it --pid=host --cgroupns=host --privileged -v /etc/os-release:/etc/os-release-host:ro -e LIBBPFGO_OSRELEASE_FILE=/etc/os-release-host aquasec/tracee:latest --rules TRC-2 --rules TRC-6 --rules TRC-7 --rules TRC-15 | ts '[%Y-%m-%d %H:%M:%S] module=Tracee output=' >> /var/log/detector.log &
 
 
 # tracee-updater
